784f5cc367e5d2b17fb774efbf3f2e34cf1eec41
bchanot <chanot.bastien@gmail.com>
2019-03-11 13:22:37 +0100

    Mettre à jour 'level05/Ressources/script05.sh'

--- level05/Ressources/script05.sh
+++ level05/Ressources/script05.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "#!/bin/bash" > /opt/openarenaserver/script.sh 
 echo "getflag > /tmp/token05" >> /opt/openarenaserver/script.sh
+echo "Now, wait 30sec max for the cron execute the file"
 
 while true; do
 	ret=`cat /tmp/token05 2> /dev/null`
