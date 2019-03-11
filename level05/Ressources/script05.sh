d18879a57c240e4c5963eaf3045f4f1951d0c944
bchanot <chanot.bastien@gmail.com>
2019-03-11 13:20:14 +0100

    Mettre à jour 'level05/Ressources/script05.sh'

--- level05/Ressources/script05.sh
+++ level05/Ressources/script05.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-getflag > /tmp/token05
+echo "#!/bin/bash" > /opt/openarenaserver/script.sh 
+echo "getflag > /tmp/token05" >> /opt/openarenaserver/script.sh
+
+while true; do
+	ret=`cat /tmp/token05 2> /dev/null`
+    if [ -n $ret ]; then
+    	break
+    fi
+done
+
+echo $ret
